e9ea479f957f09933d6f64c69f7a909d8aaa6716
Ian Percy <ipercy20@gmail.com>
2017-07-21 21:33:56 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,22 @@
+
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget(){
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n){
+  let list = document.querySelectorAll('.ranked-list li')
+  for (let i=0; i < list.length; i++){
+    let num = parseInt(list[i].innerHTML)
+    num = num + n
+    list[i].innerHTML = num
+  }
+}
+
+function deepestChild(){
+  let list = document.getElementById('grand-node').querySelectorAll('div')
+    return list[list.length-1]
+}
